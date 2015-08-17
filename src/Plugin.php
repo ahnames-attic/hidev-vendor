40b4e8616ecddc384dac857ea25ebd5867a58283
Andrii Vasyliev <sol@hiqdev.com>
2015-08-17 19:23:37 +0000

    php-cs-fixed

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * AHnames general hidev config
+ *
+ * @link      https://github.com/ahnames/hidev-config
+ * @package   hidev-config
+ * @license   No license
+ * @copyright Copyright (c) 2015, AHnames (https://ahnames.com/)
+ */
+
 namespace ahnames\hidev\config;
 
 class Plugin extends \hiqdev\pluginmanager\Plugin
