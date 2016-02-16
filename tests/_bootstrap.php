fbb7d9765ed7590630ccfc4240d4dd5e555bc06d
Andrii Vasyliev <sol@hiqdev.com>
2016-02-16 14:12:23 +0000

    inited tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * AHnames vendor configuration for HiDev
+ *
+ * @link      https://github.com/ahnames/hidev-config
+ * @package   hidev-config
+ * @license   No license
+ * @copyright Copyright (c) 2015-2016, AHnames (https://ahnames.com/)
+ */
+
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/../vendor/autoload.php';
